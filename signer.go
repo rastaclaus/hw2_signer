c467ee22fbc0d9b465ad04882c2f98c6cf37ed6a
Artem Shestopalov <tema.rastaclaus@gmail.com>
2018-10-25 21:08:14 +0300

    ..and again and again and again

--- signer.go
+++ signer.go
@@ -2,51 +2,74 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
-// сюда писать код
-
-var wg = &sync.WaitGroup{}
-
-func testPipe(in, out chan interface{}) {
-	defer wg.Done()
-	recv := <-in
-	if cnv, ok := recv.(int); ok {
-		cnv++
-		out <- cnv
-		runtime.Gosched()
+func SingleHash(in, out chan interface{}) {
+	for recv := range in {
+		if recv, ok := recv.(int); ok {
+			data := strconv.Itoa(recv)
+			out <- (DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data)))
+		}
 	}
+	close(out)
 }
 
-func swapPipe(in, out chan interface{}) {
-	defer wg.Done()
-	fmt.Println("swap")
-	recv := <-out
-	in <- recv
-	runtime.Gosched()
+func MultiHash(in, out chan interface{}) {
+	result := ""
+	for recv := range in {
+		if data, ok := recv.(string); ok {
+			for th := 0; th < 6; th++ {
+				result += DataSignerCrc32(strconv.Itoa(th) + data)
+			}
+		}
+	}
+	out <- result
+	close(out)
+	result = ""
+}
+
+func CombineResults(in, out chan interface{}) {
+	ss := make([]string, 0)
+	for recv := range in {
+		if data, ok := recv.(string); ok {
+			ss = append(ss, data)
+		}
+	}
+	sort.Strings(ss)
+	out <- strings.Join(ss, "_")
+	close(out)
 }
 
 func ExecutePipeline(workers ...job) {
-	in, out := make(chan interface{}, 1), make(chan interface{})
-	for _, worker := range workers {
+	var wg = &sync.WaitGroup{}
+	in, out := make(chan interface{}, 100), make(chan interface{}, 100)
+	for _, work := range workers {
 		wg.Add(1)
-		go worker(in, out)
-		wg.Add(1)
-		go swapPipe(in, out)
+		go func() {
+			defer wg.Done()
+			work(in, out)
+			close(out)
+		}()
+		in, out = out, make(chan interface{}, 100)
 	}
-	in <- 1
+	wg.Wait()
 }
 
 func main() {
-	workflow := []job{
-		job(testPipe),
-		job(testPipe),
-		job(testPipe),
-		job(testPipe),
-		job(testPipe),
-	}
+	workflow := make([]job, 4)
+	workflow[0] = job(func(in, out chan interface{}) {
+		for i := 0; i < 2; i++ {
+			fmt.Println(i)
+			out <- i
+		}
+		close(out)
+	})
+	workflow[1] = job(SingleHash)
+	workflow[2] = job(MultiHash)
+	workflow[3] = job(CombineResults)
 	ExecutePipeline(workflow...)
-	wg.Wait()
 }
